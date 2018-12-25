01732a207d57b7f41599b46afd22f9f1cdd3c770
Shaun Burriss <mrblah999@gmail.com>
2018-12-25 10:37:13 -0500

    Fixed latest page; broken by archive updates

--- blog/latest.php
+++ blog/latest.php
@@ -1,6 +1,10 @@
 <?php
+//Finds latest year
+$years = glob("posts/*");
+rsort($years);
+$year = $years[0];
 //Gets last modified file, which should be the latest post
-$files = array_merge(glob("posts/*.html"));
+$files = glob("$year/*/*.html");
 $files = array_combine($files, array_map("filemtime", $files));
 arsort($files);
 $latest_file = key($files);
