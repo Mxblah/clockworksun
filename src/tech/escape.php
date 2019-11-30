0977e1ac89731fe8d419598f585c2132af7cd9c6
Shaun Burriss <mrblah999@gmail.com>
2019-11-29 22:25:46 -0500

    Changed prettiness a bit

--- src/tech/escape.php
+++ src/tech/escape.php
@@ -30,18 +30,17 @@
             <?php include($_SERVER['DOCUMENT_ROOT']."/scripts/time_elapsed_string.php") ?>
 
             <!-- First Section -->
+            <div class="w3-light-grey">
             <section class="w3-container w3-content w3-row-padding w3-padding-64">
-                <div class="w3-twothird">
-                    <h5 class="cs-padding-top-32 w3-margin-right">There are <h3 class="tx-hammer"><?php
+                <div>
+                    <h2 class="tx-hammer w3-center w3-jumbo"><?php
                         $time = "Sat, 19 Dec 2020 10:30:00 -0500 (EST)";
                         echo time_elapsed_string($time, true);
-                    ?></h3> left before I graduate from Michigan Tech and can escape Houghton, MI.</h5>
+                    ?></h2>
                 </div>
-
-                <div class="w3-third w3-center">
-                    <a href="#"><img src="https://clockworksun.com/media/clockworksun-512.png" alt="Clockwork Sun" class="w3-round-xlarge w3-image w3-card-4 cs-hover-opacity-light"></a>
-                </div>
+                <h3 class="cs-padding-top-32 w3-center">remain before I graduate from college and can escape Houghton, MI.</h3>
             </section>
+            </div>
 
         </main>
 
