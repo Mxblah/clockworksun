8dc0474545796fcad1cecb6c73f845316fdd6268
Shaun Burriss <mrblah999@gmail.com>
2019-11-29 22:10:31 -0500

    Started work on graduation timer

---
+++ src/tech/escape.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- Page template version 1.2.0 -->
+    <head>
+        <title>Clockwork Sun< - How long until I escape Houghton/title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Hammersmith+One">
+        <link rel="stylesheet" href="https://clockworksun.com/stylesheets/globalStyles.css">
+    </head>
+    <body id="MainPage" class="site">
+
+        <!-- Navbar -->
+        <!--#include virtual="/navbar.html" -->
+
+        <!-- Page Content -->
+
+        <!-- Header -->
+        <header id="mainHeader" class="w3-container w3-center w3-orange w3-padding-32">
+            <h1 class="w3-xxxlarge tx-hammer"><br><b>GRADUATION TIMER</b></h1>
+            <h4 class="tx-hammer"><b>HOW LONG UNTIL I CAN ESCAPE HOUGHTON</b></h4>
+        </header>
+
+        <!-- Main page content -->
+        <main id="mainContent" class="site-content">
+
+            <!-- PHP inclusion section -->
+            <?php include($_SERVER['DOCUMENT_ROOT']."/scripts/time_elapsed_string.php") ?>
+
+            <!-- First Section -->
+            <section class="w3-container w3-content w3-row-padding w3-padding-64">
+                <div class="w3-twothird">
+                    <h5 class="cs-padding-top-32 w3-margin-right">There are <h3 class="tx-hammer"><?php
+                        $time = "Sat, 19 Dec 2020 10:30:00 -0500 (EST)";
+                        echo time_elapsed_string($time, true);
+                    ?></h3> left before I graduate from Michigan Tech and can escape Houghton, MI.</h5>
+                </div>
+
+                <div class="w3-third w3-center">
+                    <a href="#"><img src="https://clockworksun.com/media/clockworksun-512.png" alt="Clockwork Sun" class="w3-round-xlarge w3-image w3-card-4 cs-hover-opacity-light"></a>
+                </div>
+            </section>
+
+        </main>
+
+        <!-- End Page Content -->
+
+        <!-- Footer -->
+        <!--#include virtual="/footer.html" -->
+
+    </body>
+</html>
